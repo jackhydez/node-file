a1f35f4b81269bf4dfe36dc8b4b5c584c3b72f8f
jackhydez <pash.ok92@yandex.ru>
2021-01-31 20:32:49 +0300

    add upload files

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const app = require('express')();
-const PORT = process.env.PORT || 3000;
-
-var http = require('http').createServer(app);
+const express = require("express");
+const multer  = require("multer");
+  
+const app = express();
+  
+app.use(express.static(__dirname));
+app.use(multer({dest:"uploads"}).single("filedata"));
+app.post("/upload", function (req, res, next) {
+   
+    let filedata = req.file;
+    console.log(filedata);
+    if(!filedata) {
+				res.send("Ошибка при загрузке файла");
+		}
+		/*
+    else
+				res.send("Файл загружен");
+		*/
+});
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/src/index.html');
@@ -15,6 +30,6 @@ app.get('/src/app.js', (req, res) => {
 	res.sendFile(__dirname + '/src/app.js');
 });
 
-http.listen(PORT, () => {
+app.listen(3000, () => {
 	console.log('server had been started on http://localhost:3000');
 });
